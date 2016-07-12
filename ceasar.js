0c00fd49421954127e23b57bca4eb6151ce290a3
ntxtthomas <apm.tthomas@gmail.com>
2016-07-12 11:02:20 -0500

    more update

--- ceasar.js
+++ ceasar.js
@@ -1,57 +1,12 @@
-<script>
-
-// example: DAD
-
-// var alphabet = createLetters();
-// console.log(alphabet);
-
-// function createLetters() {
-//   var upper = [];
-//   for (var i = 0; i < 26; i++) {
-//     upper.push(String.fromCharCode(i+65))
-//   }
-//   return upper.concat(upper.map(function(letter) { return letter.toLowerCase() }));
-// }
-
-
-
-console.log("hello world");
-
-
-
-
-// var shift = ['abcdef'];
-// var fruit = [];
-// function caesarCipher(shift) {
-//   for(var i = 0; i < shift.length; i++){
-//     var t = shift.slice(shift.length-3, shift.length);
-//     fruit.push(t);
-
-//     // shift.slice()
-//     //.concat
-//   }
-// console.log(fruit);
-// }
-
-// function encrypt(text, shift) {
-//   // encrypt the text
-// }
-
-// function decrypt(text, shift) {
-//   // decrypt the text
-// }
-
-
-// // testing. note that we can shift no more than 52.
-// for (var i = 0; i < 53; i++) {
-//   console.log("Test Number: ", i);
-//   var text = "This is a test";
-//   var test = encrypt(text, i);
-//   console.log(test);
-//   console.log(decrypt(test, i) + "\n\n\n");
-// }
-
-// Decrypt the following message using a shift of 33:
-// mAx uxLM MABGz tuHNM t uHHExtG BL xOxG By RHN tKx PKHGz, RHN tKx HGER Hyy uR t uBM. - (TGHGRFHNL)
-
-</script>
+var shift = 'abcdef';
+	var fruit = [];
+	function caesarCipher(shift) {
+	  for(var i = 0; i < 1; i++){
+	    var t = shift.slice(shift.length-3, shift.length);
+	    var u = shift.slice(shift[0], shift.length-3);
+	    fruit = t.concat(u);
+	  }
+	return fruit;
+	}
+	var result = caesarCipher(shift);
+	console.log(result);
