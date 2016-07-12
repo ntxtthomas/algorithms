e8e7ec1d3de16576dda6f24231814401b9c1c42e
ntxtthomas <apm.tthomas@gmail.com>
2016-07-12 11:12:35 -0500

    updation

--- ceasar.js
+++ ceasar.js
@@ -1,5 +1,8 @@
+
+
 var shift = 'abcdef';
-	var fruit = [];
+var fruit = [];
+
 	function caesarCipher(shift) {
 	  for(var i = 0; i < 1; i++){
 	    var t = shift.slice(shift.length-3, shift.length);
--- greaterThanY.js
+++ greaterThanY.js
@@ -1,4 +1,15 @@
-/*	Given an array x (e.g. [1,5, 10, -2]), create an algorithm (sets of instructions) 
-that replaces any negative number with the value of 0.  When the program is done x should 
+/*	Given an array x (e.g. [1,5, 10, -2]), create an algorithm that replaces any 
+negative number with the value of 0.  When the program is done x should 
 have no negative values (e.g. [1, 5, 10, 0]).  */
+
+var x = [1,3,5,7];
+y = 3;
+var count = 0; 
+for(var i = 0; i < x.length; i++){
+	if(x[i] > y)
+	{
+		count++;
+	}	
+}
+console.log(count);
 
